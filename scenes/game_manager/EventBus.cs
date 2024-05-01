1d847436a223da6abe3add93a8e8ff60e462222c
Bentebent <henriksson.tim@gmail.com>
2024-05-01 20:54:17 +0200

    refactor: autoload gamemanager singleton

--- scenes/game_manager/EventBus.cs
+++ scenes/game_manager/EventBus.cs
@@ -1,14 +1,32 @@
 using System;
+using core;
 using Godot;
 
 namespace wmg
 {
     public partial class EventBus : Node
     {
+        private core.EventHandler _eventHandler = new core.EventHandler();
+
         // Called when the node enters the scene tree for the first time.
         public override void _Ready() { }
 
         // Called every frame. 'delta' is the elapsed time since the previous frame.
         public override void _Process(double delta) { }
+
+        public void Subscribe(string eventName, EventDelegate callback)
+        {
+            _eventHandler.Subscribe(eventName, callback);
+        }
+
+        public void Unsubscribe(string eventName, EventDelegate callback)
+        {
+            _eventHandler.Unsubscribe(eventName, callback);
+        }
+
+        public void Publish(string eventName, params object[] args)
+        {
+            _eventHandler.Publish(eventName, args);
+        }
     }
 }
--- scenes/game_manager/GameManager.cs
+++ scenes/game_manager/GameManager.cs
@@ -8,8 +8,17 @@ namespace wmg
         [Export]
         public EventBus EventBus { get; private set; }
 
+        [Export]
+        public DecisionEngine DecisionEngine { get; private set; }
+
+        [Export]
+        public UIManager UIManager { get; private set; }
+
         // Called when the node enters the scene tree for the first time.
-        public override void _Ready() { }
+        public override void _Ready()
+        {
+            GD.Print("GameManager Ready");
+        }
 
         // Called every frame. 'delta' is the elapsed time since the previous frame.
         public override void _Process(double delta) { }
